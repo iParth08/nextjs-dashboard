fbbe5da5c722e972be23c4313141411c1e358de4
Shwet Prakash <krish.shwetprakash08@gmail.com>
2024-12-09 03:18:32 +0530

    CRUD invoice

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,7 +1,27 @@
-import React from "react";
+import Form from "@/app/ui/invoices/edit-form";
+import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
+import { fetchCustomers, fetchInvoiceById } from "@/app/lib/data";
 
-const page = () => {
-  return <div>page</div>;
-};
+export default async function Page(props: { params: Promise<{ id: string }> }) {
+  const id = (await props.params).id;
+  const [invoice, customers] = await Promise.all([
+    fetchInvoiceById(id),
+    fetchCustomers(),
+  ]);
 
-export default page;
+  return (
+    <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: "Invoices", href: "/dashboard/invoices" },
+          {
+            label: "Edit Invoice",
+            href: `/dashboard/invoices/${id}/edit`,
+            active: true,
+          },
+        ]}
+      />
+      <Form invoice={invoice} customers={customers} />
+    </main>
+  );
+}
